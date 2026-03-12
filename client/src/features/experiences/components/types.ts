6f6bf2531d4ea52fedc99e2b47c818cf08873c51
deputatovn <deputatovn@yandex.ru>
2026-03-12 22:20:30 +0300

    Add ExperienceList and ExperienceCard components for displaying experiences

---
+++ client/src/features/experiences/components/types.ts
@@ -0,0 +1,11 @@
+import { Experience, User } from "@advanced-react/server/database/schema";
+
+type ExperienceWithUser = Experience & {
+  user: User;
+};
+
+type ExperienceWithCommentsCount = Experience & {
+    commentsCount: number
+}
+
+export type ExperienceForList = ExperienceWithUser & ExperienceWithCommentsCount;
