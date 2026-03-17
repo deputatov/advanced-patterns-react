b561fac29a0fd50f9061ce19ebd2b29642439fa3
deputatovn <deputatovn@yandex.ru>
2026-03-17 22:25:53 +0300

    Add CommentCreateForm component and integrate it into CommentsSection

--- client/src/features/comments/components/CommentsSection.tsx
+++ client/src/features/comments/components/CommentsSection.tsx
@@ -2,6 +2,7 @@ import { Experience } from "@advanced-react/server/database/schema";
 
 import { trpc } from "@/trpc";
 
+import { CommentCreateForm } from "./CommentsCreaterForm";
 import CommentsList from "./CommentsList";
 
 type CommentsSectionProps = {
@@ -27,6 +28,8 @@ export function CommentsSection({
   return (
     <div className="space-y-4">
       <h3 className="font-semibold">Comments ({commentsCount})</h3>
+
+      <CommentCreateForm experienceId={experienceId} />
 
       <CommentsList
         comments={commentsQuery.data ?? []}
